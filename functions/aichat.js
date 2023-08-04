e0b34385ad5037f954b69bb156cab6abce0679a6
Matan Dessaur <matandessaur@m8ndoes.com>
2023-08-04 12:52:13 -0400

    testing web integration

--- functions/aichat.js
+++ functions/aichat.js
@@ -1,53 +1,14 @@
 const axios = require('axios');
 
-const fetchGoogleSearchResults = async (query) => {
-  try {
-    const response = await axios.get(`https://www.googleapis.com/customsearch/v1`, {
-      params: {
-        key: process.env.GOOGLE_SEARCH_API_KEY,
-        cx: process.env.CUSTOM_SEARCH_ENGINE_ID,
-        q: query
-      }
-    });
-
-    // Add additional logging here
-    console.log("Full response data from Google API: ", response.data);
-
-    if (response.data.items) {
-      const firstResult = response.data.items[0];
-      console.log(firstResult);
-      return `${firstResult.title}: ${firstResult.snippet}`;
-    }
-
-    return null;
-  } catch (error) {
-    console.error("Error in fetchGoogleSearchResults: ", error);
-    return null;
-  }
-};
-
-
 exports.handler = async function(event, context) {
   try {
     const conversationHistory = event.queryStringParameters.history;
     const userInput = event.queryStringParameters.input;
-    const searchResult = await fetchGoogleSearchResults("Services Videotron");
-
-    
-    if (!searchResult) {
-      // handle the case when no search results are returned
-      return {
-        statusCode: 200,
-        body: JSON.stringify({ output: `I couldn't find any information about that, can you please ask something else?` }),
-      };
-    }
-
-    // Include the search result as part of the AI's prompt
     const prompt = `
-    Le suivant est une conversation entre un agent de vente de télécommunication et un assistant virtuel nommé VIA. L'assistant virtuel est programmé pour aider l'agent de vente à répondre aux questions des clients, et à fournir des informations sur les produits et services de l'entreprise videotron.
+    Le suivant est une conversation entre un agent de vente de télécommunication chez videotron et un assistant virtuel nommé VIA. L'assistant virtuel est programmé pour aider l'agent de vente à répondre a ses questions et à fournir des informations sur les produits et services de l'entreprise videotron.
     ${conversationHistory}
     \nAgent de vente: ${userInput}
-    \nVIA: Voici mes recherces sous une liste de liens, ${searchResult}.`;
+    \nVIA:`;
     const response = await axios.post("https://api.openai.com/v1/engines/text-davinci-003/completions", 
       {
         prompt: prompt,
