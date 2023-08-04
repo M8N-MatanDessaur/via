268b52ac6fab30bd17c46b90577f5977a4738fb6
Matan Dessaur <matandessaur@m8ndoes.com>
2023-08-04 12:20:39 -0400

    testing web integration

--- functions/aichat.js
+++ functions/aichat.js
@@ -1,39 +1,15 @@
-const axios = require('axios');
-
-const fetchGoogleSearchResults = async (query) => {
-  try {
-    const response = await axios.get(`https://www.googleapis.com/customsearch/v1`, {
-      params: {
-        key: process.env.GOOGLE_SEARCH_API_KEY,
-        cx: process.env.CUSTOM_SEARCH_ENGINE_ID,
-        q: query
-      }
-    });
-
-    if (response.data.items) {
-      const firstResult = response.data.items[0];
-      console.log(firstResult);
-      return `${firstResult.title}: ${firstResult.snippet}`;
-    }
-
-    return null;
-  } catch (error) {
-    console.error(error);
-    return null;
-  }
-}
-
 exports.handler = async function(event, context) {
   try {
     const conversationHistory = event.queryStringParameters.history;
     const userInput = event.queryStringParameters.input;
     const searchResult = await fetchGoogleSearchResults('Videotron services');
+
+    // Include the search result as part of the AI's prompt
     const prompt = `
-    Voici les informations trouvées: ${searchResult}
     Le suivant est une conversation entre un agent de vente de télécommunication et un assistant virtuel nommé VIA. L'assistant virtuel est programmé pour aider l'agent de vente à répondre aux questions des clients, et à fournir des informations sur les produits et services de l'entreprise videotron.
     ${conversationHistory}
     \nAgent de vente: ${userInput}
-    \nVIA:`;
+    \nVIA: D'après mes recherches, ${searchResult}.`;
     const response = await axios.post("https://api.openai.com/v1/engines/text-davinci-003/completions", 
       {
         prompt: prompt,
